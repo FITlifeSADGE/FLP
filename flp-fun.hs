cedacc9bbe2001a5cc2f0fa4cca6ec9b3dedf872
FITlifeSADGE <lukas8967@gmail.com>
2024-03-11 14:46:26 +0100

    Finished 1 prolly

--- flp-fun.hs
+++ flp-fun.hs
@@ -3,51 +3,9 @@ import System.Environment
 import Data.List.Split
 import Data.List
 import Debug.Trace (trace)
+import Data.Maybe (catMaybes)
 
-type NodeData = (String, String, Int)
-
-transformList :: [(String, Int)] -> [NodeData]
-transformList = map parseElement
-
-parseElement :: (String, Int) -> NodeData
-parseElement (s, lvl) = case words s of
-    ("Node:":n:value:_) -> ("Node", value, lvl) -- Hodnota je nyní řetězcem
-    ("Leaf:":cls) -> ("Leaf", unwords cls, lvl)
-    _ -> error "Unrecognized pattern"
-
--- findLeaf :: [NodeData] -> [Double] -> Maybe String
--- findLeaf nodes numbers = findLeafHelper nodes numbers 0 where
---   findLeafHelper :: [NodeData] -> [Double] -> Int -> Maybe String
---   findLeafHelper [] _ _ = Nothing
---   findLeafHelper _ [] _ = Nothing
---   findLeafHelper nodes@(current@(typ,value,level):xs) (n:ns) skips
---     | typ == "Node" && read value < n && skips == 0 = 
---         trace ("Node " ++ value ++ " is smaller, looking for a second node at level " ++ show (level + 1)) $ findLeafHelper xs (n:ns) 1
---     | typ == "Node" && read value >= n && skips == 0 = 
---         trace ("Node " ++ value ++ " is not smaller, looking for leaf at level " ++ show (level + 1)) $ findLeafHelper xs ns 0
---     | typ == "Leaf" && skips > 0 = 
---         trace ("Skipping Leaf " ++ value ++ " at level " ++ show level) $ findLeafHelper xs (n:ns) (skips - 1)
---     | typ == "Leaf" && skips == 0 = 
---         trace ("Found Leaf " ++ value ++ " at level " ++ show level) $ Just value
---     | otherwise = findLeafHelper xs (n:ns) skips
-
-findLeaf :: [NodeData] -> [Double] -> Maybe String
-findLeaf nodes numbers = findLeafHelper nodes (numbers ++ [0]) 0 where
-  findLeafHelper :: [NodeData] -> [Double] -> Int -> Maybe String
-  findLeafHelper [] _ _ = Nothing
-  findLeafHelper _ [] _ = Nothing
-  findLeafHelper nodes@(current@(typ,value,level):xs) numbers@(n:ns) skips
-    | typ == "Node" && read value < n && skips == 0 = 
-        trace ("Node " ++ value ++ " is smaller, moving to next number and looking for a second node at level " ++ show (level + 1)) $ findLeafHelper xs ns 1
-    | typ == "Node" && read value >= n && skips == 0 = 
-        trace ("Node " ++ value ++ " is not smaller, moving to next number and looking for leaf at level " ++ show (level + 1)) $ findLeafHelper xs ns 0
-    | typ == "Leaf" && skips > 0 = 
-        trace ("Skipping Leaf " ++ value ++ " at level " ++ show level) $ findLeafHelper xs numbers (skips - 1)
-    | typ == "Leaf" && skips == 0 = 
-        trace ("Found Leaf " ++ value ++ " at level " ++ show level) $ Just value
-    | otherwise = findLeafHelper xs numbers skips
-
-
+type NodeData = (String, Int, String, Int)
 
 -- Čte text ze souboru
 loadFile :: FilePath -> IO String
@@ -61,6 +19,43 @@ rozdelText text = splitOn "\n" text
 pridejUroven :: [String] -> [(String, Int)]
 pridejUroven a = map (\a -> (a, length (takeWhile (==' ') a) `div` 2)) a
 
+transformList :: [(String, Int)] -> [NodeData]
+transformList = map parseElement
+
+parseElement :: (String, Int) -> NodeData
+parseElement (s, lvl) =
+  let (prefix:rest) = words s
+      (nodeType, restString) = case prefix of
+        "Node:" -> ("Node", unwords rest)
+        "Leaf:" -> ("Leaf", unwords rest)
+        _ -> error "Unrecognized pattern"
+      splitRest = splitOn ", " restString
+      number = if nodeType == "Node" then read (head splitRest) :: Int else -1 -- Leaf nemá index příznaku
+      value = if nodeType == "Node" then splitRest !! 1 else head splitRest
+  in (nodeType, number, value, lvl)
+
+findLeaf :: [NodeData] -> [Double] -> String
+findLeaf nodes numbers = findLeafHelper nodes numbers 0 0 where
+  findLeafHelper :: [NodeData] -> [Double] -> Int -> Int -> String
+  findLeafHelper [] _ _ _ = error "No leaf found"
+  findLeafHelper _ [] _ _ = error "No leaf found"
+  findLeafHelper nodes@((typ,index,value,level):xs) numbers skips levelToFind
+    | typ == "Node" && comparisonNumber >= read value && skips == 0 && level == levelToFind = 
+        findLeafHelper xs numbers 1 (level + 1)
+    | typ == "Node" && comparisonNumber < read value && skips == 0 && level == levelToFind = 
+        findLeafHelper xs numbers 0 (level + 1)
+    | typ == "Leaf" && skips > 0 && level >= levelToFind = 
+        findLeafHelper xs numbers (skips - 1) levelToFind
+    | typ == "Leaf" && skips == 0 && level == levelToFind = 
+         value
+    | otherwise = findLeafHelper xs numbers skips levelToFind
+    where comparisonNumber = numbers !! index
+
+convertToDoubleLists :: String -> [[Double]]
+convertToDoubleLists contents = do
+  doubleList <- map (map read . splitOn ",") $ lines contents
+  return doubleList
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -68,12 +63,10 @@ main = do
         ("-1":treeFile:newDataFile:_) -> do -- První podúkol
             treeContents <- loadFile treeFile
             newDataContents <- loadFile newDataFile
-            putStrLn "Data ze souboru obsahujici strom:"
-            --print (pridejUroven (rozdelText treeContents))
-            print (transformList (pridejUroven (rozdelText treeContents)))
-            --putStrLn "Data ze souboru obsahujici nove data:"
-            --putStrLn newDataContents
-            print (findLeaf (transformList (pridejUroven (rozdelText treeContents))) ([6,3.1]))
+            let transformedList = transformList (pridejUroven (rozdelText treeContents))
+            let doubleList = convertToDoubleLists newDataContents
+            let result = map (findLeaf transformedList) doubleList
+            print result
         ("-2":trainingDataFile:_) -> do -- Druhý podúkol
             trainingDataContents <- loadFile trainingDataFile
             putStrLn "Data ze souboru obsahujici trenovaci data:"
